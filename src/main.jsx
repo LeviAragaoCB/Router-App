d32924996d97148eebdc4e070ee98655896e3abc
LEVI ARAGAO DE CASTRO BERNARDO <levibernardo0753@digitalcollege.internal>
2025-09-02 20:18:30 -0300

    Config de Rotas with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import { Login } from './Pages/Login.jsx';
+import { Cadastro } from './Pages/Cadastro.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* <App /> */}
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
