bb9a03167cbd313763fa6f2cb1e32a848f2a4831
LEVI ARAGAO DE CASTRO BERNARDO <levibernardo0753@digitalcollege.internal>
2025-09-02 22:01:37 -0300

    Colocando Navigate

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,44 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Login } from './Pages/Login.jsx';
 import { Cadastro } from './Pages/Cadastro.jsx';
+import { NotFound } from './Pages/NotFound.jsx';
 
+// Versão 6.30.1 *Angelo acha que é a melhor versão.
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />
+  },
+  {
+    path: '/home',
+    element: <App />
+  },
+  {
+    path: '/cadastro',
+    element: <Cadastro />
+  },
+  {
+    path: '*',
+    element: <NotFound />
+  },
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+   <RouterProvider router={router} /> 
+
+    {/* Versão 7.8.2 
     <BrowserRouter>
-      {/* <App /> */}
+      <App />
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<App />} />
         <Route path="/cadastro" element={<Cadastro />} />
+        404
+        <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter> */}
   </StrictMode>,
 )
