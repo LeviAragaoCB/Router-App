e267ec7a4b95886e5388021288db03972c9ae5a1
LEVI ARAGAO DE CASTRO BERNARDO <levibernardo0753@digitalcollege.internal>
2025-09-09 20:17:39 -0300

    Finalizando Login

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
 import { Navbar } from './Navbar'
 import { Footer } from './Footer'
+import { Outlet } from 'react-router-dom'
 
 export const Layout = () => {
     return (
         <>
-
+            <Navbar />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Login } from './pages/Login.jsx';
 import { Cadastro } from './pages/Cadastro.jsx';
 import { NotFound } from './pages/NotFound.jsx';
